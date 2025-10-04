b68d9d0c068a16520ffc4487d5e687f7b98a5698
Yg0R2 <yg0r2@yahoo.com>
2025-10-04 19:30:51 +0200

    feature: add java project conventions

---
+++ conventions/src/main/kotlin/org.eazyportal.plugin.gradle.java-project-conventions.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    java
+    `java-test-fixtures`
+}
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
