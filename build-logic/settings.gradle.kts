f1d002bf03881789d16c6c3d1644b81803bc8804
Yg0R2 <yg0r2@yahoo.com>
2025-10-22 01:51:31 +0200

    feature: use version catalog

--- build-logic/settings.gradle.kts
+++ build-logic/settings.gradle.kts
@@ -5,3 +5,11 @@ project(":conventions").apply {
     name = "build-logic-conventions"
     projectDir = file("../conventions")
 }
+
+dependencyResolutionManagement {
+    versionCatalogs {
+        create("libs") {
+            from(files("../gradle/libs.versions.toml"))
+        }
+    }
+}
--- build-logic/src/main/kotlin/gradle-plugins-conventions.gradle.kts
+++ build-logic/src/main/kotlin/gradle-plugins-conventions.gradle.kts
@@ -4,17 +4,19 @@ plugins {
     id("org.eazyportal.plugin.gradle.repositories-conventions")
 }
 
+val libs = extensions.getByType<VersionCatalogsExtension>().named("libs")
+
 dependencies {
     // Test-Fixtures dependencies
-    testFixturesImplementation(platform("org.assertj:assertj-bom:3.27.6"))
-    testFixturesImplementation(platform("org.junit:junit-bom:6.0.0"))
+    testFixturesImplementation(platform(libs.findLibrary("assertj-bom").get()))
+    testFixturesImplementation(platform(libs.findLibrary("junit-bom").get()))
 
     testFixturesImplementation("org.assertj:assertj-core")
     testFixturesImplementation("org.junit.jupiter:junit-jupiter")
 
     // Test dependencies
-    testImplementation(platform("org.assertj:assertj-bom:3.27.6"))
-    testImplementation(platform("org.junit:junit-bom:6.0.0"))
+    testImplementation(platform(libs.findLibrary("assertj-bom").get()))
+    testImplementation(platform(libs.findLibrary("junit-bom").get()))
 
     testImplementation("org.assertj:assertj-core")
     testImplementation("org.junit.jupiter:junit-jupiter")
