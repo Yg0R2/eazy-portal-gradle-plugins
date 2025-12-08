722d0700c0dd2893df7d013e6aa94cde229d1edf
Yg0R2 <yg0r2@yahoo.com>
2025-12-08 14:53:07 +0100

    baseline

--- common/src/testFixtures/kotlin/org/eazyportal/plugin/common/junit/JunitExtensions.kt
+++ common/src/testFixtures/kotlin/org/eazyportal/plugin/common/junit/JunitExtensions.kt
@@ -3,6 +3,7 @@ package org.eazyportal.plugin.common.junit
 import org.junit.jupiter.api.Named
 import kotlin.reflect.KClass
 
+@Deprecated("not needed anymore")
 fun <T : Any> classNamed(payload: T): Named<T> =
     if (payload is KClass<*>) {
         payload
